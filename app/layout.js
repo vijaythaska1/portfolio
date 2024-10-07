3ca58b06e86de5780790b604a5c0185911f54608
Vijay Sharma <vijaysharma@cqlsys.co.uk>
2024-10-07 12:18:02 +0530

    isue doen

--- app/layout.js
+++ app/layout.js
@@ -8,16 +8,23 @@ import "./css/card.scss";
 import "./css/globals.scss";
 import ScrollToTop from "./components/helper/scroll-to-top";
 const inter = Inter({ subsets: ["latin"] });
+import Head from "next/head";
 
 export const metadata = {
   title: "Portfolio of Vijay Sharma - Software Developer",
-  description:
-    "This is the portfolio of Vijay Sharma. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+  description:"This is the portfolio of Vijay Sharma. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="description" content={metadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="Vijay Sharma"/>
+        <title>{metadata.title}</title>
+        <link rel="icon" href="favicon.ico" />
+      </Head>
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
