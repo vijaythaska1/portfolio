2be47dc7f83fc0c470bd098fcbc4654d8aab68d3
Vijay Sharma <vijaysharma@cqlsys.co.uk>
2024-10-07 18:37:06 +0530

    bgfhvjfd

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -4,7 +4,9 @@ import { CgGitFork } from "react-icons/cg";
 import { IoStar } from "react-icons/io5";
 
 function Footer() {
-  return (
+  return ( 
+
+    
     <div className="relative border-t bg-[#0d1224] border-[#353951] text-white">
       <div className="mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] py-6 lg:py-10">
         <div className="flex justify-center -z-40">
